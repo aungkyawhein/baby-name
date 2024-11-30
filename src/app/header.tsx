1771247956fa9f7eefae57364352b9d101c44b16
aungkyawhein <akh.infotech@gmail.com>
2024-12-01 00:03:47 +1300

    Update styles

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <h1 className="text-lg text-cyan-800">
+    <h1 className="text-lg text-zinc-600 dark:text-zinc-200">
       <a href='/'>Baby Name</a>
     </h1>
   )
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -44,13 +44,13 @@ export default function Main() {
 
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center">
-      <h3 className="text-xl text-stone-800">
+      <h3 className="text-xl text-stone-800 dark:text-stone-200 border border-solid border-zinc-600 dark:border-zinc-400 px-12 py-3 rounded-full">
         { name }
       </h3>
 
       <div className="flex gap-4 items-center flex-col sm:flex-row">
         <button
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 px-4"
           onClick={handleClick}
         >
           Generate
